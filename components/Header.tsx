3fdd7fa0d8da412d3ac0c547f245732a788ccadb
thomas-coldwell <tcoldwell72@gmail.com>
2021-07-20 18:39:14 +0100

    Created menu for nav bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,13 @@
 import InstagramLogo from "../public/instagram.svg";
 import Link from "next/link";
 import Image from "next/image";
-import { MdFavoriteBorder } from "react-icons/md";
+import { MdArrowDropDown, MdFavoriteBorder } from "react-icons/md";
 import { Button } from "./Button";
 import { supabase } from "../lib/supabase";
 import { useRouter } from "next/router";
 import { useCurrentUser } from "../data/queries/useCurrentUser";
 import { useMemo } from "react";
+import { useState } from "react";
 
 interface IHeaderProps {
   showAddPost?: boolean;
@@ -15,6 +16,8 @@ interface IHeaderProps {
 export const Header = (props: IHeaderProps) => {
   //
   const { showAddPost } = props;
+
+  const [menuVisible, setMenuVisible] = useState(false);
 
   const router = useRouter();
 
@@ -41,10 +44,10 @@ export const Header = (props: IHeaderProps) => {
         )}
         <Link href="/likes">
           <a>
-            <MdFavoriteBorder className="w-8 h-8 mr-4 text-gray-600" />
+            <MdFavoriteBorder className="w-8 h-8 mr-4 text-gray-500" />
           </a>
         </Link>
-        <div className="relative w-10 h-10">
+        <div className="relative w-10 h-10 mr-2">
           <Link href={`/user/${user?.username}`}>
             <a className="absolute w-full h-full overflow-hidden bg-gray-300 rounded-full">
               {user && (
@@ -57,6 +60,32 @@ export const Header = (props: IHeaderProps) => {
               )}
             </a>
           </Link>
+        </div>
+        <div className="relative">
+          <button onClick={() => setMenuVisible((vis) => !vis)}>
+            <MdArrowDropDown className="w-8 h-8 text-gray-500" />
+          </button>
+          {menuVisible && (
+            <div className="absolute right-0 mt-4 overflow-hidden rounded-md shadow">
+              <button
+                className="w-full px-4 py-2 text-sm font-medium text-left whitespace-nowrap hover:bg-blue-400 hover:text-white"
+                onClick={() => {
+                  router.push(`/user/${user?.username}`);
+                }}
+              >
+                My Profile
+              </button>
+              <button
+                className="w-full px-4 py-2 text-sm font-medium text-left whitespace-nowrap hover:bg-red-400 hover:text-white"
+                onClick={async () => {
+                  await supabase.auth.signOut();
+                  router.replace("/auth");
+                }}
+              >
+                Logout
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
