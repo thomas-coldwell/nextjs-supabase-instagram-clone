a8a473e4b221c727611dcfb000fe14468419a3e5
Thomas Coldwell <tcoldwell72@gmail.com>
2021-09-25 13:02:50 +0100

    fix: Battling TRPC endpoint env

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -42,9 +42,10 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : "http://localhost:3000/api/trpc";
+    const url =
+      process.env.NODE_ENV !== "development"
+        ? `${window.location.origin}/api/trpc`
+        : "http://localhost:3000/api/trpc";
 
     return {
       links: [httpLink({ url })],
